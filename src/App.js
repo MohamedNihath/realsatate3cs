65c43ceabe6066a3a0ebeed8318ca2bea8ad53fb
MohamedNihath <mohamednihath0308@gmail.com>
2024-03-30 17:07:34 +0530

    Create Companies Component And Modify App.js File

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
+import Companies from './components/Companies/Companies';
 
 
 
@@ -16,7 +17,7 @@ const App = () => {
           <Header/>
           <Hero/>
         </div>
-        
+        <Companies/>
         
    </div>
 
