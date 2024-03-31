a23fa6d10161d346671a12d8bf8e923821947c5a
MohamedNihath <mohamednihath0308@gmail.com>
2024-03-31 13:32:20 +0530

    Created Residencis Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Companies from './components/Companies/Companies';
+import Residencis from './components/Residencis/Residencis';
 
 
 
@@ -18,7 +19,7 @@ const App = () => {
           <Hero/>
         </div>
         <Companies/>
-
+        <Residencis/>
         
         
    </div>
